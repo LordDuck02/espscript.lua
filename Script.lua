ac5ab2c7fb65558f54ff15604707dd71bd83ebb8
LordDuck02 <97839276+LordDuck02@users.noreply.github.com>
2023-01-26 09:11:37 -0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,12 +1,13 @@
 local players = {}
 local chams = {}
+local espEnabled = true
 
 function give_esp(plr)
   local cham = Instance.new("Highlight", game:GetService("CoreGui"))
   table.insert(chams, cham)
   
   game:GetService("RunService").Stepped:Connect(function()
-        cham.Enabled = true
+        cham.Enabled = espEnabled
         cham.OutlineTransparency = 0
         cham.OutlineColor = Color3.fromRGB(3,33,46)
         cham.Adornee = plr.Character
@@ -30,12 +31,12 @@ table.remove(players, v)
 end)
 
 
-function remove_esp()
+function toggle_esp()
+    espEnabled = not espEnabled
     for _,cham in pairs(chams) do
-        cham:Destroy()
+        cham.Enabled = espEnabled
     end
-    chams = {}
 end
 
--- keybind thingy, rember for future scripps
-game:GetService("ContextActionService"):BindAction("remove_esp", remove_esp, false, Enum.KeyCode.Z)
+-- if you're reading this, u can change 'Z' to any letter u want to (must be caps)
+game:GetService("ContextActionService"):BindAction("toggle_esp", toggle_esp, false, Enum.KeyCode.Z)
