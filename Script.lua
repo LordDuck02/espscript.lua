4e90e8f2f7fed664014f0fca3a468cdcfa2097dc
LordDuck02 <97839276+LordDuck02@users.noreply.github.com>
2023-01-26 10:13:12 -0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -2,41 +2,29 @@ local players = {}
 local chams = {}
 local espEnabled = true
 
-function give_esp(plr)
+game:GetService("Players").PlayerAdded:Connect(function(plr)
   local cham = Instance.new("Highlight", game:GetService("CoreGui"))
   table.insert(chams, cham)
-  
-  game:GetService("RunService").Stepped:Connect(function()
-        cham.Enabled = espEnabled
-        cham.OutlineTransparency = 0
-        cham.OutlineColor = Color3.fromRGB(3,33,46)
-        cham.Adornee = plr.Character
-        cham.FillColor = Color3.fromRGB(100,3,5)
-        cham.FillTransparency = 0
-   end)
-end
-
-for _, v in pairs(game:GetService("Players"):GetPlayers()) do
-     table.insert(players, v)
-     give_esp(v)
-end
-
-game:GetService('Players').PlayerAdded:connect(function(v)
-table.insert(players, v)
-give_esp(v)
+  cham.Enabled = espEnabled
+  cham.OutlineTransparency = 0
+  cham.OutlineColor = Color3.fromRGB(3,33,46)
+  cham.Adornee = plr.Character
+  cham.FillColor = Color3.fromRGB(100,3,5)
+  cham.FillTransparency = 0
+  table.insert(players, plr)
 end)
 
-game:GetService('Players').PlayerRemoving:connect(function(v)
-table.remove(players, v)
+game:GetService("Players").PlayerRemoving:Connect(function(plr)
+  local cham = table.remove(chams, plr)
+  cham:Destroy()
+  table.remove(players, plr)
 end)
-
 
 function toggle_esp()
     espEnabled = not espEnabled
-    for _,cham in pairs(chams) do
+    for _, cham in pairs(chams) do
         cham.Enabled = espEnabled
     end
 end
 
--- if you're reading this, u can change 'Z' to any letter u want to (must be caps)
 game:GetService("ContextActionService"):BindAction("toggle_esp", toggle_esp, true, Enum.KeyCode.Z)
